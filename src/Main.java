e2604ab70071ece52da2e4640dfbafb5d47f02ae
Lukasz Gancarz <Lukasz.Gancarz@gds.ey.com>
2023-03-02 14:07:30 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<Integer>();
+        List<Integer> numbers = new ArrayList<>();
         do {
             System.out.println("Enter a number from 1 to 1000 ('END' to stop):");
             String str = input.nextLine();
@@ -23,26 +23,41 @@ public class Main {
         } while (true);
         Collections.sort(numbers);
         int length = numbers.size();
-        int thirdDecile = (int) Math.ceil(length * 0.3) - 1;
-        int sixthDecile = (int) Math.ceil(length * 0.6) - 1;
+        int secoundDecile = (int) 2*(length+1)/10;
+        int thirdDecile = (int) 3*(length+1)/10;
+        int fifthDecile = (int) 5*(length+1)/10;
+        int sixthDecile = (int) 6*(length+1)/10;
         int thirdDecileCount = 0;
+        List<Integer> third = new ArrayList<>();
+        List<Integer> sixth = new ArrayList<>();
         int sixthDecileCount = 0;
-        System.out.println(thirdDecile);
-        System.out.println(sixthDecile);
-        for (int i = 0; i < length; i++) {
-            int numAtIndex = numbers.get(i);
-            if (i == thirdDecile) {
+        int sum = 0;
+        int secoundDecileNumber = (int) numbers.get(Math.round(secoundDecile));
+        int thirdDecileNumber = (int) numbers.get(Math.round(thirdDecile));
+        int fifthDecileNumber = (int) numbers.get(Math.round(fifthDecile));
+        int sixthDecileNumber = (int) numbers.get(Math.round(sixthDecile));
+        for(int i = 0; i < length - 1; i++) {
+            sum += numbers.get(i);
+            if(i > secoundDecileNumber - 1 && i <= thirdDecileNumber - 1) {
                 thirdDecileCount++;
-            } else if (i == sixthDecile) {
+                third.add(numbers.get(i));
+            }
+            if(i > fifthDecileNumber - 1 && i <= sixthDecileNumber) {
                 sixthDecileCount++;
-            } else if (numAtIndex > numbers.get(thirdDecile) && numAtIndex <= numbers.get(sixthDecile)) {
-                thirdDecileCount++;
-                sixthDecileCount++;
+                sixth.add(numbers.get(i));
             }
         }
-
-        System.out.printf("Total numbers entered: %d\n", length);
-        System.out.printf("Numbers in the 3rd decile: %d\n", thirdDecileCount);
-        System.out.printf("Numbers in the 6th decile: %d\n", sixthDecileCount);
+        System.out.println("3rd decile:");
+        System.out.println(third);
+        System.out.println("6th decile");
+        System.out.println(sixth);
+        System.out.println("Minumum:");
+        System.out.println(numbers.get(0));
+        System.out.println("Maximum:");
+        System.out.println(numbers.get(length-1));
+        System.out.println("Range:");
+        System.out.println(numbers.get(length-1)-numbers.get(0));
+        System.out.println("Sum:");
+        System.out.println(sum);
     }
 }
