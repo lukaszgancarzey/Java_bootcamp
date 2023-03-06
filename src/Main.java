df9624636c8a59740216af2981b3ce5e4c1a7797
Lukasz Gancarz <Lukasz.Gancarz@gds.ey.com>
2023-03-06 11:24:47 +0100

    review fixes

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,19 @@ public class Main {
         do {
             System.out.println("Enter a number from 1 to 1000 ('END' to stop):");
             String str = input.nextLine();
-            int number = Integer.parseInt(str);
-            try {
-                if (number >= 1 && number <= 1000) {
-                    numbers.add(number);
-                } else if (str.equals("END")){
-                    break;
-                } else {
-                    System.out.println("Invalid input");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Insert number value");
+            if (str.equals("END")) {
+                break;
+            } if (Integer.parseInt(str) >= 1 && Integer.parseInt(str) <= 1000) {
+                numbers.add(Integer.parseInt(str));
+            } else {
+                System.out.println("Invalid input");
             }
         } while (true);
-        Collections.sort(numbers);
-        int length = numbers.size();
+        CalculateDecile(numbers);
+    }
+    public static void CalculateDecile(List<Integer> args) {
+        Collections.sort(args);
+        int length = args.size();
         int secoundDecile = (int) 2*(length+1)/10;
         int thirdDecile = (int) 3*(length+1)/10;
         int fifthDecile = (int) 5*(length+1)/10;
@@ -33,19 +31,19 @@ public class Main {
         List<Integer> sixth = new ArrayList<>();
         int sixthDecileCount = 0;
         int sum = 0;
-        int secoundDecileNumber = (int) numbers.get(secoundDecile);
-        int thirdDecileNumber = (int) numbers.get(thirdDecile);
-        int fifthDecileNumber = (int) numbers.get(fifthDecile);
-        int sixthDecileNumber = (int) numbers.get(sixthDecile);
+        int secoundDecileNumber = (int) args.get(secoundDecile);
+        int thirdDecileNumber = (int) args.get(thirdDecile);
+        int fifthDecileNumber = (int) args.get(fifthDecile);
+        int sixthDecileNumber = (int) args.get(sixthDecile);
         for(int i = 0; i < length - 1; i++) {
-            sum += numbers.get(i);
+            sum += args.get(i);
             if(i > secoundDecileNumber - 1 && i <= thirdDecileNumber - 1) {
                 thirdDecileCount++;
-                third.add(numbers.get(i));
+                third.add(args.get(i));
             }
             if(i > fifthDecileNumber - 1 && i <= sixthDecileNumber) {
                 sixthDecileCount++;
-                sixth.add(numbers.get(i));
+                sixth.add(args.get(i));
             }
         }
         System.out.println("3rd decile:");
@@ -53,11 +51,11 @@ public class Main {
         System.out.println("6th decile");
         System.out.println(sixth);
         System.out.println("Minumum:");
-        System.out.println(numbers.get(0));
+        System.out.println(args.get(0));
         System.out.println("Maximum:");
-        System.out.println(numbers.get(length-1));
+        System.out.println(args.get(length-1));
         System.out.println("Range:");
-        System.out.println(numbers.get(length-1)-numbers.get(0));
+        System.out.println(args.get(length-1)-args.get(0));
         System.out.println("Sum:");
         System.out.println(sum);
     }
